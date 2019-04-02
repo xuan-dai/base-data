74889e84ad1b15929a2ead75fdef9aa7afdcd07e
daixuan <337568659@qq.com>
2019-04-02 22:51:02 +0800

    initialize repository

---
+++ base-data-server/src/main/java/cn/gorillahug/base/data/server/BaseDataServerApplication.java
@@ -0,0 +1,13 @@
+package cn.gorillahug.base.data.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseDataServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseDataServerApplication.class, args);
+    }
+
+}
